a32dd031d984b12cdabd3cf3e0999462d5e77070
patrysia <patrycja@example.com>
2017-06-19 21:07:24 +0200

    Added files: index.html, scripts.js ad style.css

---
+++ scripts.js
@@ -0,0 +1,6 @@
+﻿//Script of the triangle area
+var a = 7;
+var h = 14;
+var triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
